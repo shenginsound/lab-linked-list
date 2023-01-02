b71c0f0e9a6f52dfcdc4b2c2c17dec6d70f8c10e
Albert Lionelle <albert.lionelle@colostate.edu>
2023-01-01 18:15:30 -0700

    added ll template

---
+++ linkedlist.c
@@ -0,0 +1,18 @@
+// Modify this file
+// compile with: gcc linkedlist.c -o linkedlist.out
+
+#include <stdio.h>
+#include <stdlib.h> // contains the functions free/malloc
+
+// TODO: Create your node_t type here
+
+// TODO: Write your functions here
+// There should be 1.) create_list 2.) print_list 3.) free_list
+// You may create as many helper functions as you like.
+
+int main()
+{
+    // TODO: Implement me!
+
+    return 0;
+}
